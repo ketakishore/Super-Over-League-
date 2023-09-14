181cadb16bde68937b58506439499ad8bfd98a56
ketakishore <ketakishore@gmail.com>
2023-09-14 04:04:00 +0000

    image test case failed

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  //   Write your code here.
+  <div className="Main_bg">
+    <h1 className="Title">Super over league</h1>
+    <div className="Image_container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="image"
+      ></img>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="image"
+      ></img>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
